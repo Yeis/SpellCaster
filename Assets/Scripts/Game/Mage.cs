53ba3e9f5ad3e1d71fe80a4c56336c3076d9f77d
Rodolfo Hernandez <rodolfo@Pascal.local>
2020-10-20 19:25:30 -0700

    Adding maguito prefab and handling movement

--- Assets/Scripts/Game/Mage.cs
+++ Assets/Scripts/Game/Mage.cs
@@ -6,8 +6,12 @@ public class Mage : MonoBehaviour
 {
     public string name = "MrYeis";
     public float health = 50;
+    public float speed = 5;
     private List<Spell> spellBook;
-    // Start is called before the first frame update
+    private float t;
+    private Vector2 currDirectiion;
+
+    public Animator animator;
     void Start()
     {
         spellBook = new List<Spell>();
@@ -16,12 +20,38 @@ public class Mage : MonoBehaviour
 
         // spellBook.Add(fira);
         // spellBook.Add(heal);
-
+        t = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+        //     currDirectiion = Direction.Left;
+        //     animator.SetFloat("Horizontal", 1);
+        //     animator.SetFloat("Vertical", 0);
+        // }
+        // else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+        //     currDirectiion = Direction.Right;
+        //     animator.SetFloat("Horizontal", -1);
+        //     animator.SetFloat("Vertical", 0);
+
+        // } else {
+        //     animator.SetFloat("Horizontal", 0);
+        //     animator.SetFloat("Vertical", 0);
+        //     currDirectiion = Vector2.zero;
+        // }
+
+        animator.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal"));
+        animator.SetFloat("Vertical", Input.GetAxisRaw("Vertical"));
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0.0f);
+        
+        transform.position = transform.position + (movement * speed) * Time.deltaTime;
+
+        // t += Time.deltaTime / 3f;
+        // transform.position = Vector2.MoveTowards(transform.position, (Vector2)transform.position + currDirectiion, t);
+
+
 
     }
 }
